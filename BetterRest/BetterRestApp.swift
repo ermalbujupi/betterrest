ec10642d83ddd063c91c56c5c944f656d7b6fdc4
Ermal Bujupaj <ermal.bj@gmail.com>
2023-01-18 23:30:02 +0100

    first commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ermal Bujupaj on 18.01.23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
