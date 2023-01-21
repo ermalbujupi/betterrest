d8d4ea5f1a499e316075f64bbb6ba9c8f8af3b34
Ermal Bujupaj <ermal.bj@gmail.com>
2023-01-21 16:03:54 +0100

    Add function to calculate bedtime from prediction model

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -25,6 +25,25 @@ struct ContentView: View {
         return Calendar.current.date(from: components) ?? Date.now
     }
     
+    func calculateBedTime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 & 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is…"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
         
         showingAlert = true
     }
